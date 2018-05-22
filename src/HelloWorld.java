e3dd4e717b02356f9461313282c18ad5fe3fbe5a
Ri <Ri@Profs-MacBook-Air.local>
2018-05-22 16:01:47 -0400

    Print x3 added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Hello Eclipse!!");
+		System.out.printf("Hello Eclipse 2!!");
+		System.out.print("R");
 	}
 
 }
