6f0ec3630f39f1aba42fc49038abd31ceee836c0
Ri <Ri@Profs-MacBook-Air.local>
2018-05-22 16:31:16 -0400

    Commented one print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Eclipse!!");
 		System.out.printf("Hello Eclipse 2!!");
-		System.out.print("R");
+//		System.out.print("R");
 	}
 
 }
