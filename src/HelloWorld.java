991d527cdeb0c845ee414e4f4f4fca0a7a15b3c3
Ri <Ri@Profs-MacBook-Air.local>
2018-05-23 13:34:36 -0400

    Comment two printstatements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,9 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Eclipse!!");
-		System.out.printf("Hello Eclipse 2!!");
+//		System.out.printf("Hello Eclipse 2!!");
 //		System.out.print("R");
+		
 	}
 
 }
